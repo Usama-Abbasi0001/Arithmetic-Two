8f95d318df919c6d83942c065a92af1f3e6959c3
Usama-Abbasi0001 <157971981+Usama-Abbasi0001@users.noreply.github.com>
2024-03-17 13:29:10 +0500

    Arithmetic

---
+++ first.js
@@ -0,0 +1,109 @@
+//simple code
+document.write("this is my first program <br>");
+
+// //three types of Variable var,let,const
+var myname ="M.Usama Abbsi <br>";
+document.write(myname);
+
+var myage = 40 ;
+document.write(myage );
+
+let myfather ="<br> M.hussain";
+document.write(myfather);
+
+const my ="<br> Abbasi";
+document.write(my);
+
+const mycast ="<br> program";
+document.write(mycast);
+document.write('<br> <br>');
+
+//data types string
+var x ="data types";
+document.write(x);
+document.write(' <br>');
+document.write(typeof x);
+
+document.write('<br> <br>');
+
+//data types Numbmer
+var x = 45 ;
+document.write(x);
+document.write(' <br>');
+document.write(typeof x);
+
+document.write('<br> <br>');
+
+//data types Boolean
+var x = true;
+document.write(x);
+document.write(' <br>');
+document.write(typeof x);
+
+document.write('<br> <br>');
+
+//data types Arry
+var x = ["apple", "Mango", "Bananna",];
+document.write(x);
+document.write(' <br>');
+document.write(typeof x);
+
+document.write('<br> <br>');
+
+//data types Arry
+var x = {
+    firstname:"usasma",
+    secondname:"abbasi"
+};
+document.write(x);
+document.write(' <br>');
+document.write(typeof x);
+
+
+//airthmetic operator
+document.write('<br> <br>');
+document.write('Arithmetic Operators <br>');
+var a = 9;
+var b = 3;
+var c = a * b;
+document.write(c);
+
+//Assignment operator
+document.write('<br> <br>');
+document.write('Assignment Operators <br>');
+var a = 9;
+var b = 3;
+a /=  b;
+document.write(a);
+
+//console with arithmetic subtraction
+document.write('<br> <br>');
+document.write('console one <br>');
+var a = 98;
+var b = 10;
+console.log(a+b);
+
+
+//console with arithmetic multipilcation
+document.write('<br> <br>');
+document.write('console Two <br>');
+var a = 23;
+var b = 7;
+console.log(a*b);
+
+
+//console with Arry
+document.write('<br> <br>');
+document.write('console with Arry <br>');
+var a = 23;
+var b = 7;
+console.log([4,7,1,2]);
+
+
+//console with arithmetic table
+document.write('<br> <br>');
+document.write('console with table <br>');
+var a = 23;
+var b = 7;
+console.table([7,5,8,10]);
+
